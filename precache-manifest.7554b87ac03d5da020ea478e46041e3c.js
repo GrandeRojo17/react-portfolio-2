33098790f11d3a7118454c1d7a7d69202cba4bd0
GrandeRojo17 <38540605+GrandeRojo17@users.noreply.github.com>
2020-08-11 14:38:58 -0500

    Updates

---
+++ precache-manifest.7554b87ac03d5da020ea478e46041e3c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac5fd645489ce88128029fa072bdb8d",
+    "url": "/react-portfolio-2/index.html"
+  },
+  {
+    "revision": "a55be09dc81f0e171353",
+    "url": "/react-portfolio-2/static/css/main.95e449fc.chunk.css"
+  },
+  {
+    "revision": "a6264f9db756904dd975",
+    "url": "/react-portfolio-2/static/js/2.e8334190.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio-2/static/js/2.e8334190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a55be09dc81f0e171353",
+    "url": "/react-portfolio-2/static/js/main.5d4c6f4b.chunk.js"
+  },
+  {
+    "revision": "126dd8b58e8287e1a3ff",
+    "url": "/react-portfolio-2/static/js/runtime-main.d309cc5c.js"
+  },
+  {
+    "revision": "08565fc9fe7b06cff44f3766ef4b10c2",
+    "url": "/react-portfolio-2/static/media/fitnessTracker_Pic02.08565fc9.PNG"
+  },
+  {
+    "revision": "bbaa29412160f06454cc6ec916fab00b",
+    "url": "/react-portfolio-2/static/media/snapchat_bitmoji_clear_transparent_faceOnly_cropped.bbaa2941.png"
+  },
+  {
+    "revision": "2cdd6cee16aa368f016eec7bd3f44380",
+    "url": "/react-portfolio-2/static/media/weatherDash_01.2cdd6cee.PNG"
+  }
+]);
